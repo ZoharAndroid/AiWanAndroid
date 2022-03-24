0a62bf0f0d37accc9579c6a28cd21419b3adea30
ZoharAndroid <1048132071@qq.com>
2022-03-24 15:14:18 +0800

    create toolbar

--- app/src/main/java/com/zzh/aiwanandroid/MainActivity.java
+++ app/src/main/java/com/zzh/aiwanandroid/MainActivity.java
@@ -1,10 +1,17 @@
 package com.zzh.aiwanandroid;
 
 import androidx.annotation.NonNull;
+import androidx.appcompat.app.ActionBar;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
+import androidx.core.view.GravityCompat;
+import androidx.drawerlayout.widget.DrawerLayout;
 import androidx.fragment.app.Fragment;
 
+import android.annotation.SuppressLint;
 import android.os.Bundle;
+import android.view.Gravity;
+import android.view.Menu;
 import android.view.MenuItem;
 import android.widget.FrameLayout;
 
@@ -19,7 +26,7 @@ public class MainActivity extends AppCompatActivity {
 
     private BottomNavigationView mBottomView;
     private FrameLayout mContentGroupFrameLayout;
-
+    private DrawerLayout mDrawerLayout;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -28,10 +35,27 @@ public class MainActivity extends AppCompatActivity {
 
         mBottomView = findViewById(R.id.bottom_navigation);
         mContentGroupFrameLayout = findViewById(R.id.content_group);
+        Toolbar toolbar = findViewById(R.id.toolbar);
+        mDrawerLayout = findViewById(R.id.drawer_layout_main);
 
+        // 将toolbar设置Actionbar
+        setSupportActionBar(toolbar);
+
+        // 给标题栏添加新的图标
+        ActionBar actionBar = getSupportActionBar();
+        if (actionBar != null) {
+            // 隐藏系统自带的图标
+            actionBar.setDisplayHomeAsUpEnabled(true);
+            // 设置新的图标
+            actionBar.setHomeAsUpIndicator(R.mipmap.icon_menu_nav);
+        }
+
+        // 默认开启Home页面
         Fragment mHomeFragment = HomeFragment.getInstance(null, null);
         getSupportFragmentManager().beginTransaction().add(R.id.content_group, mHomeFragment).commit();
 
+
+        // 给底部导航栏添加点击事件
         mBottomView.setOnNavigationItemSelectedListener(new BottomNavigationView.OnNavigationItemSelectedListener() {
             @Override
             public boolean onNavigationItemSelected(@NonNull MenuItem item) {
@@ -59,6 +83,34 @@ public class MainActivity extends AppCompatActivity {
 
     }
 
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.tool_bar_menu, menu);
+        return true;
+    }
+
+    @SuppressLint("NonConstantResourceId")
+    @Override
+    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
+        switch (item.getItemId()) {
+            // 点击toolbar的home按钮
+            case android.R.id.home:
+                mDrawerLayout.openDrawer(GravityCompat.START);
+                break;
+            case R.id.menu_share:
+                // 分享
+                break;
+            case R.id.menu_collection:
+                // 收藏
+
+                break;
+            case R.id.menu_explorer:
+                // 浏览器打开
+
+                break;
+        }
+        return true;
+    }
 
     /**
      * 切换Fragment
